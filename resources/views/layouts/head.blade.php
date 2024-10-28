2e651365c4aa8693eb9a3d6c17573e3db961f27f
Prasetyo Putra Pratama <Prastyopratama01@gmail.com>
2024-10-28 11:02:29 +0700

    update meta

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,31 +9,11 @@
 <link rel="stylesheet" href="https://hianime.io/css/styles.min.css?v=1.0">
 <!--End: Stylesheet-->
 
-
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="robots" content="index,follow" />
-<meta http-equiv="content-language" content="en" />
-<meta name="description"
-    content="Watch your favorite anime online in Dub or Sub format without registration on aniwatchtv.to fastest Streaming server NOW." />
-<meta name="keywords"
-    content="watch anime online, anime site, free anime, anime to watch, online anime, anime streaming, stream anime online, english anime, english dubbed anime" />
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://aniwatchtv.to/home" />
-<meta property="og:title" content="Aniwatch Free Anime Streaming Homepage" />
-<meta property="og:image" content="https://hianime.to/images/capture.png" />
-<meta property="og:image:width" content="650">
-<meta property="og:image:height" content="350">
-<meta property="og:description"
-    content="Watch your favorite anime online in Dub or Sub format without registration on aniwatchtv.to fastest Streaming server NOW." />
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-<meta name="apple-mobile-web-app-status-bar" content="#202125">
-<meta name="theme-color" content="#202125">
 <link rel="shortcut icon" href="{{ asset('assets/images/favicon/favicon.png?v=0.1') }}" type="image/x-icon" />
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/candu.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/candu.png">
 <link rel="mask-icon" href="/safari-pinned-tab.svg?v=0.1" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
 <link rel="icon" sizes="192x192" href="/assets/images/candu.png">
 <link rel="icon" sizes="512x512" href="/assets/images/candu.png">
 <script src="https://hianime.io/js/jquery.min.js" type="text/javascript"></script>
