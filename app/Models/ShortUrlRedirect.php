6c25e733c15981a2b1afa020741c0a9bd2acb1e0
Prasetyo Putra Pratama <Prastyopratama01@gmail.com>
2024-11-11 10:09:41 +0700

    update seo

--- app/Models/ShortUrlRedirect.php
+++ app/Models/ShortUrlRedirect.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShortUrlRedirect extends Model
 {
-    protected $fillable = ['code', 'url', 'hits'];
+    protected $fillable = ['code', 'url', 'hits', 'film_id'];
+
+    public function film(){
+        return $this->belongsTo(Film::class);
+    }
 }
